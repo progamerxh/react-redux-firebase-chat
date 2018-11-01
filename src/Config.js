5ea18720c438dfd271a3b5becacd3f038e267312
Ku Hiệp <vipmrak97h@gmail.com>
2018-11-01 18:09:46 +0700

    Connect firebase store

---
+++ src/Config.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyAKD6P1MJSBCWqh6ZamRz5tldWtxL2trnk",
+    authDomain: "react-chat-c506e.firebaseapp.com",
+    databaseURL: "https://react-chat-c506e.firebaseio.com",
+    projectId: "react-chat-c506e",
+    storageBucket: "",
+    messagingSenderId: "606265220385"
+};
