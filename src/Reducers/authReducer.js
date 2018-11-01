bb1d6af083585a8da60c3344ad9674e48b95364d
Ku Hiệp <vipmrak97h@gmail.com>
2018-11-01 19:18:31 +0700

    Create authReducer

---
+++ src/Reducers/authReducer.js
@@ -0,0 +1,53 @@
+import * as types from '../Actions/authActionTypes';
+
+const initialState = {
+  isUserSignedIn: false,
+  isInProgress: false,
+  hasError: false,
+  errorMessage: '',
+  photoURL: '',
+  displayName: '',
+  uid: 0
+};
+
+export function auth(state = initialState, action) {
+  const { user } = action;
+  switch (action.type) {
+    case types.FETCH_USER:
+      if (user)
+        return {
+          ...state,
+          isUserSignedIn: true,
+          isInProgress: false,
+          photoURL: user.photoURL,
+          uid: user.uid,
+          displayName: user.displayName
+        }
+      else
+        return state;
+    case types.SIGNIN_SUCCESS:
+      return {
+        ...state,
+        isUserSignedIn: true,
+        isInProgress: false,
+        photoURL: user.photoURL,
+        uid: user.uid,
+        displayName: user.displayName
+      };
+    case types.SIGNIN:
+      return {
+        ...state,
+        isInProgress: true
+      };
+    case types.SIGNIN_ERROR:
+      const { errorMessage } = action;
+      return {
+        ...state,
+        hasError: true,
+        errorMessage
+      };
+    case types.LOG_OUT:
+    default:
+      return state;
+  }
+}
