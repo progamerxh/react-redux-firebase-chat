5a35e905a9081265128711bd33ba00a93cc2c993
Ku Hiệp <vipmrak97h@gmail.com>
2018-11-01 18:30:29 +0700

    Update App.css

--- src/Actions/authActions.js
+++ src/Actions/authActions.js
@@ -20,3 +20,56 @@ function signInError(errorMessage) {
     errorMessage
   }
 }
+
+export function logout() {
+  return (dispatch, getState) => {
+    firebase.database().ref().child(`users/${getState().auth.uid}`).update({
+      isActive: false,
+      lastTimeLoggedIn: firebase.database.ServerValue.TIMESTAMP
+    });
+    dispatch({
+      type: types.LOG_OUT,
+    });
+    firebase.auth().signOut();
+  }
+}
+
+export const fetchUser = () => dispatch => {
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      dispatch({
+        type: types.FETCH_USER,
+        user
+      });
+    } else {
+      dispatch({
+        type: types.FETCH_USER,
+        user: null
+      });
+    }
+  });
+};
+
+export function signIn() {
+  return (dispatch) => {
+    dispatch(signInInProgress());
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+      .then((result) => {
+        const { user: { uid, displayName, photoURL, email } } = result;
+        firebase.database().ref(`users/${uid}`).set({
+          isActive: true,
+          displayName,
+          photoURL,
+          email,
+          lastTimeLoggedIn: firebase.database.ServerValue.TIMESTAMP
+        });
+        dispatch(
+          signInSuccess(result.user)
+        );
+      })
+      .catch((error) => {
+        dispatch(signInError(error.message))
+      });
+  }
+}
