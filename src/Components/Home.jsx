8e48fbdf8736db2efa481428677eba5e49ccb0ca
Ku Hiệp <vipmrak97h@gmail.com>
2018-11-03 21:17:27 +0700

    Update Home component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-
+import Message from './Message';
+import RoomList from './RoomList';
 import Tags from './Tags';
+import UserList from './UserList';
 import SearchForm from './SearchForm';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import '../App.css'
@@ -19,10 +21,17 @@ class Home extends Component {
             <SearchForm onUserLogin={this.handleUserLogin} />
             <Switch>
               <Route path='/home' component={Tags} />
+              <Route path='/room/:roomname' component={UserList} />
+              <Route path='/inbox/' component={UserList} />
             </Switch>
           </div>
           <div className="right">
             <InforPanel />
+            <Switch>
+              <Route path='/home' component={RoomList} />
+              <Route path='/room/:roomname' component={Message} />} />
+            <Route path='/inbox/:touserid' component={Message} />
+            </Switch>
           </div>
         </div>
       </BrowserRouter>
