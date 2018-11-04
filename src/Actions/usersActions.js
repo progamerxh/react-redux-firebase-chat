cc9eec6c46b743f34f08d90c20c4000a689a8fa6
Ku Hiệp <vipmrak97h@gmail.com>
2018-11-05 00:51:55 +0700

    add Favorite method to usersActions

--- src/Actions/usersActions.js
+++ src/Actions/usersActions.js
@@ -1,10 +1,17 @@
 import * as types from './usersActionTypes';
 
 export const retrieveUser = (uid, user) => {
-  return {
-    type: types.RETRIEVE_USER,
-    uid,
-    user
+  return (dispatch, getState) => {
+    var favList = getState().favList;
+    for (let i = 0; i < favList.length; i++) {
+      if (favList[i] === uid)
+        user.isFav = true;
+    }
+    user.uid = uid;
+    dispatch({
+      type: types.RETRIEVE_USER,
+      user
+    })
   }
 };
 
