2b2d30fd9dd5315551a0bd09d4e5793a65546ae2
Ku Hiệp <vipmrak97h@gmail.com>
2018-11-11 13:20:45 +0700

    Add sending state when send image

--- src/Components/MessageForm.jsx
+++ src/Components/MessageForm.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { firebaseConnect } from 'react-redux-firebase'
 import { compose } from 'redux'
 import { connect } from 'react-redux';
+import Loading from './Loading';
 
 class MessageForm extends Component {
     constructor(props) {
@@ -10,9 +11,11 @@ class MessageForm extends Component {
             text: '',
             file: null,
             previewImgUrl: null,
+            isSending: false,
         };
         this.submitHandler = this.submitHandler.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.removeFileHandler = this.removeFileHandler.bind(this);
     }
     generatePreviewImgUrl(file, callback) {
         const reader = new FileReader();
@@ -46,50 +49,62 @@ class MessageForm extends Component {
             this.submitHandler(e);
         }
     }
+
+    submitMessage(filename) {
+        const { auth, messageThread, firebase, userInbox } = this.props;
+        const createdAt = Date.now();
+        const message = {
+            message: this.state.text,
+            createdAt,
+            photoURL: auth.photoURL,
+            filename: filename,
+            displayName: auth.displayName,
+            uid: auth.uid
+        };
+        firebase.push(`messages/${messageThread}`, message);
+        if (userInbox) {
+            firebase.set(`lastmessages/${auth.uid}/${userInbox.uid}`,
+                {
+                    createdAt, message: message.message, user: {
+                        avatarUrl: userInbox.avatarUrl,
+                        displayName: userInbox.displayName,
+                        email: userInbox.email,
+                        uid: userInbox.uid,
+                    }
+                });
+            firebase.set(`lastmessages/${userInbox.uid}/${auth.uid}`,
+                {
+                    createdAt, message: message.message, user: {
+                        avatarUrl: auth.photoURL,
+                        displayName: auth.displayName,
+                        email: auth.email,
+                        uid: auth.uid
+                    }
+                });
+        }
+        this.setState({ file: null, previewImgUrl: null, text: '' });
+    }
+
     submitHandler(e) {
         e.preventDefault();
         if (this.state.text.trim() !== "" | this.state.file != null) {
-            const { auth, messageThread, firebase, userInbox } = this.props;
-            const createdAt = Date.now();
             const file = this.state.file;
             var filename = null;
             if (file) {
+                this.setState({isSending: true});
                 filename = file.name;
                 const filesPath = `images/`;
-                this.props.firebase.uploadFile(filesPath, file, filesPath);
-            }
-            const message = {
-                message: this.state.text,
-                createdAt,
-                photoURL: auth.photoURL,
-                filename: filename,
-                displayName: auth.displayName,
-                uid: auth.uid
-            };
-            firebase.push(`messages/${messageThread}`, message);
-            if (userInbox) {
-                firebase.set(`lastmessages/${auth.uid}/${userInbox.uid}`,
-                    {
-                        createdAt, message: message.message, user: {
-                            avatarUrl: userInbox.avatarUrl,
-                            displayName: userInbox.displayName,
-                            email: userInbox.email,
-                            uid: userInbox.uid,
-                        }
-                    });
-                firebase.set(`lastmessages/${userInbox.uid}/${auth.uid}`,
-                    {
-                        createdAt, message: message.message, user: {
-                            avatarUrl: auth.photoURL,
-                            displayName: auth.displayName,
-                            email: auth.email,
-                            uid: auth.uid
-                        }
-                    });
-            }
+                this.props.firebase
+                    .uploadFile(filesPath, file)
+                    .then(snap => {
+                        this.submitMessage(filename);
+                        this.setState({ file: null, previewImgUrl: null, text: '', isSending:false });
+                    })
 
+            }
+            else
+                this.submitMessage(filename);
         }
-        this.setState({ file: null, previewImgUrl: null, text: '' });
     }
 
     render() {
@@ -100,9 +115,11 @@ class MessageForm extends Component {
                 {(this.state.previewImgUrl) ? (
                     <div className="previewimage">
                         <img src={this.state.previewImgUrl} />
-                        <i className="mdi-navigation-close remove"
-                            onClick={this.removeFileHandler.bind(this)}
-                        ></i>
+                        {(this.state.isSending) ? <Loading /> :
+                            <i className="mdi-navigation-close remove"
+                                onClick={this.removeFileHandler}
+                            ></i>
+                        }
                     </div>
                 ) : null
                 }
