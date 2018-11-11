1ee1c670cd49e42de213524cc065a75dade67a80
Ku Hiệp <vipmrak97h@gmail.com>
2018-11-11 18:10:51 +0700

    Add show image when add link to Message

--- src/Components/MessageRecieved.jsx
+++ src/Components/MessageRecieved.jsx
@@ -8,6 +8,16 @@ export class MessageRecieved extends Component {
             imgsrc: null,
         };
     }
+
+    componentDidMount() {
+        var text = this.props.message.message;
+        var urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+        text.replace(urlRegex, (url) => {
+            if (url.match(/\.(jpeg|jpg|gif|png)/) !== null)
+            this.setState({imgsrc:url});
+        });
+    }
+
     render() {
         const { fromMe, continous, isShowTime, message, timestone, firebase } = this.props;
 
@@ -23,14 +33,14 @@ export class MessageRecieved extends Component {
                     {(isShowTime) ? timestone : null}
                 </div>
                 <img className="avt" src={message.photoURL}></img>
-                <div className='content'>
+                <div className='content' >
                     <h2>  {message.displayName} </h2>
                     {(message.message) ?
-                        <p className="message">{message.message}</p>
+                        <p className="message"> {message.message}</p>
                         : null}
                     {(this.state.imgsrc) ? (
                         <a href={this.state.imgsrc} target="_blank">
-                            <img id="photo" src={this.state.imgsrc}
+                            <img id="photo" alt={this.state.imgsrc} src={this.state.imgsrc}
                             ></img>
                         </a>
                     ) : null}
